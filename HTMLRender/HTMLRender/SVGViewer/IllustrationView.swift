b82d3f4a2429ac99851ecbd24be6a01cc6f49230
Srinivas Attili <srinivasattili84@gmail.com>
2026-09-01 10:34:04 +0530

    sr

---
+++ HTMLRender/HTMLRender/SVGViewer/IllustrationView.swift
@@ -0,0 +1,18 @@
+//
+//  IllustrationView.swift
+//  HTMLRender
+//
+//  Created by Attili Naga Srinivasu on 01/09/26.
+//
+
+import SwiftUI
+
+struct IllustrationView: View {
+
+    var body: some View {
+
+        SVGViewer()
+            .navigationTitle("Illustration")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
